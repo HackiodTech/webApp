550369ecb659a26feb4fa206345d0c2b981fc621
HackiodTech <136133966+HackiodTech@users.noreply.github.com>
2024-07-01 19:43:31 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+import requests
+
+app = Flask(__name__)
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    visitor_name = request.args.get('visitor_name', 'Visitor')
+    client_ip = request.remote_addr
+
+    # Get the location of the client
+    location_response = requests.get(f'http://ip-api.com/json/{client_ip}')
+    location_data = location_response.json()
+    city = location_data.get('city', 'Unknown location')
+
+    # Get the weather information
+    api_key = 'YOUR_OPENWEATHERMAP_API_KEY'
+    weather_response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric')
+    weather_data = weather_response.json()
+    temperature = weather_data['main']['temp']
+
+    greeting = f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {city}"
+
+    return jsonify({
+        'client_ip': client_ip,
+        'location': city,
+        'greeting': greeting
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
